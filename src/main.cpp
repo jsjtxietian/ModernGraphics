e814f8b134c659246cce31c739278bc48ff61533
jsjtxietian <jsjtxietian@outlook.com>
2022-08-15 14:33:52 +0800

    add glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,63 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
 
 #include <stdio.h>
 #include <stdlib.h>
 
+using glm::mat4;
+using glm::vec3;
+
 static const char* shaderCodeVertex = R"(
 #version 460 core
+layout(std140, binding = 0) uniform PerFrameData
+{
+	uniform mat4 MVP;
+	uniform int isWireframe;
+};
 layout (location=0) out vec3 color;
-const vec2 pos[3] = vec2[3](
-	vec2(-0.6, -0.4),
-	vec2( 0.6, -0.4),
-	vec2( 0.0,  0.6)
+const vec3 pos[8] = vec3[8](
+	vec3(-1.0,-1.0, 1.0),
+	vec3( 1.0,-1.0, 1.0),
+	vec3( 1.0, 1.0, 1.0),
+	vec3(-1.0, 1.0, 1.0),
+
+	vec3(-1.0,-1.0,-1.0),
+	vec3( 1.0,-1.0,-1.0),
+	vec3( 1.0, 1.0,-1.0),
+	vec3(-1.0, 1.0,-1.0)
 );
-const vec3 col[3] = vec3[3](
-	vec3( 1.0, 0.0, 0.0 ),
-	vec3( 0.0, 1.0, 0.0 ),
-	vec3( 0.0, 0.0, 1.0 )
+const vec3 col[8] = vec3[8](
+	vec3( 1.0, 0.0, 0.0),
+	vec3( 0.0, 1.0, 0.0),
+	vec3( 0.0, 0.0, 1.0),
+	vec3( 1.0, 1.0, 0.0),
+
+	vec3( 1.0, 1.0, 0.0),
+	vec3( 0.0, 0.0, 1.0),
+	vec3( 0.0, 1.0, 0.0),
+	vec3( 1.0, 0.0, 0.0)
+);
+const int indices[36] = int[36](
+	// front
+	0, 1, 2, 2, 3, 0,
+	// right
+	1, 5, 6, 6, 2, 1,
+	// back
+	7, 6, 5, 5, 4, 7,
+	// left
+	4, 0, 3, 3, 7, 4,
+	// bottom
+	4, 5, 1, 1, 0, 4,
+	// top
+	3, 2, 6, 6, 7, 3
 );
 void main()
 {
-	gl_Position = vec4(pos[gl_VertexID], 0.0, 1.0);
-	color = col[gl_VertexID];
+	int idx = indices[gl_VertexID];
+	gl_Position = MVP * vec4(pos[idx], 1.0);
+	color = isWireframe > 0 ? vec3(0.0) : col[idx];
 }
 )";
 
@@ -34,84 +71,121 @@ void main()
 };
 )";
 
-int main( void )
+struct PerFrameData
+{
+	mat4 mvp;
+	int isWireframe;
+};
+
+int main(void)
 {
 	glfwSetErrorCallback(
-		[]( int error, const char* description )
+		[](int error, const char* description)
 		{
-			fprintf( stderr, "Error: %s\n", description );
+			fprintf(stderr, "Error: %s\n", description);
 		}
 	);
 
-	if ( !glfwInit() )
-		exit( EXIT_FAILURE );
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
 
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 6 );
-	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow( 1024, 768, "Simple example", nullptr, nullptr );
-	if ( !window )
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "Simple example", nullptr, nullptr);
+	if (!window)
 	{
 		glfwTerminate();
-		exit( EXIT_FAILURE );
+		exit(EXIT_FAILURE);
 	}
 
 	glfwSetKeyCallback(
 		window,
-		[]( GLFWwindow* window, int key, int scancode, int action, int mods )
+		[](GLFWwindow* window, int key, int scancode, int action, int mods)
 		{
-			if ( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS )
-				glfwSetWindowShouldClose( window, GLFW_TRUE );
+			if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+				glfwSetWindowShouldClose(window, GLFW_TRUE);
 		}
 	);
 
-	glfwMakeContextCurrent( window );
-	gladLoadGL( glfwGetProcAddress );
-	glfwSwapInterval( 1 );
+	glfwMakeContextCurrent(window);
+	gladLoadGL(glfwGetProcAddress);
+	glfwSwapInterval(1);
 
-	const GLuint shaderVertex = glCreateShader( GL_VERTEX_SHADER );
-	glShaderSource( shaderVertex, 1, &shaderCodeVertex, nullptr );
-	glCompileShader( shaderVertex );
+	const GLuint shaderVertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(shaderVertex, 1, &shaderCodeVertex, nullptr);
+	glCompileShader(shaderVertex);
 
-	const GLuint shaderFragment = glCreateShader( GL_FRAGMENT_SHADER );
-	glShaderSource( shaderFragment, 1, &shaderCodeFragment, nullptr );
-	glCompileShader( shaderFragment );
+	const GLuint shaderFragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(shaderFragment, 1, &shaderCodeFragment, nullptr);
+	glCompileShader(shaderFragment);
 
 	const GLuint program = glCreateProgram();
-	glAttachShader( program, shaderVertex );
-	glAttachShader( program, shaderFragment );
+	glAttachShader(program, shaderVertex);
+	glAttachShader(program, shaderFragment);
+	glLinkProgram(program);
+	glUseProgram(program);
 
-	glLinkProgram( program );
-	glUseProgram( program );
-
-	//vertex array object
 	GLuint vao;
-	glCreateVertexArrays( 1, &vao );
-	glBindVertexArray( vao );
+	glCreateVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-	glClearColor( 1.0f, 1.0f, 1.0f, 1.0f );
+	const GLsizeiptr kBufferSize = sizeof(PerFrameData);
 
-	while ( !glfwWindowShouldClose( window ) )
+	// Direct-State-Access
+	GLuint perFrameDataBuffer;
+	glCreateBuffers(1, &perFrameDataBuffer);
+	// The GL_DYNAMIC_STORAGE_BIT parameter tells the OpenGL implementation that the content of 
+	// the data store might be updated after creation through calls to glBufferSubData()
+	glNamedBufferStorage(perFrameDataBuffer, kBufferSize, nullptr, GL_DYNAMIC_STORAGE_BIT);
+	//binds a range within a buffer object to an indexed buffer target. 
+	glBindBufferRange(GL_UNIFORM_BUFFER, 0, perFrameDataBuffer, 0, kBufferSize);
+
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_POLYGON_OFFSET_LINE);
+	// Polygon offset is needed to render a wireframe image of the cube on top of the solid
+	// image without Z-fighting. The values of -1.0 will move the wireframe rendering
+	// slightly toward the camera.
+	glPolygonOffset(-1.0f, -1.0f);
+
+	while (!glfwWindowShouldClose(window))
 	{
 		int width, height;
-		glfwGetFramebufferSize( window, &width, &height );
-		glViewport( 0, 0, width, height );
-		glClear( GL_COLOR_BUFFER_BIT );
-		
-		//The glDrawArrays() function can be invoked with the empty VAO that bound earlier
-		//invoked with the empty VAO that we bound earlier :
-		glDrawArrays( GL_TRIANGLES, 0, 3 );
-		glfwSwapBuffers( window );
+		glfwGetFramebufferSize(window, &width, &height);
+		const float ratio = width / (float)height;
+
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		const mat4 m = glm::rotate(glm::translate(mat4(1.0f), vec3(0.0f, 0.0f, -3.5f)), (float)glfwGetTime(), vec3(1.0f, 1.0f, 1.0f));
+		const mat4 p = glm::perspective(45.0f, ratio, 0.1f, 1000.0f);
+
+		PerFrameData perFrameData = { .mvp = p * m, .isWireframe = false };
+
+		glNamedBufferSubData(perFrameDataBuffer, 0, kBufferSize, &perFrameData);
+		// render the solid cube with the polygon mode set to GL_FILL
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		perFrameData.isWireframe = true;
+		glNamedBufferSubData(perFrameDataBuffer, 0, kBufferSize, &perFrameData);
+		//render the wireframe cube using the GL_LINE polygon mode and the -1.0 polygon offset 
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	glDeleteProgram( program );
-	glDeleteShader( shaderFragment );
-	glDeleteShader( shaderVertex );
-	glDeleteVertexArrays( 1, &vao );
+	glDeleteBuffers(1, &perFrameDataBuffer);
+	glDeleteProgram(program);
+	glDeleteShader(shaderFragment);
+	glDeleteShader(shaderVertex);
+	glDeleteVertexArrays(1, &vao);
 
-	glfwDestroyWindow( window );
+	glfwDestroyWindow(window);
 	glfwTerminate();
 
 	return 0;
