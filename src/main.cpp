a5a72a596d98a2396b6b334da34ea2a68272071a
jsjtxietian <jsjtxietian@outlook.com>
2022-08-15 14:53:38 +0800

    add stb image support

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
+#include <filesystem>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,82 +15,48 @@
 using glm::mat4;
 using glm::vec3;
 
-static const char* shaderCodeVertex = R"(
+static const char *shaderCodeVertex = R"(
 #version 460 core
 layout(std140, binding = 0) uniform PerFrameData
 {
 	uniform mat4 MVP;
-	uniform int isWireframe;
 };
-layout (location=0) out vec3 color;
-const vec3 pos[8] = vec3[8](
-	vec3(-1.0,-1.0, 1.0),
-	vec3( 1.0,-1.0, 1.0),
-	vec3( 1.0, 1.0, 1.0),
-	vec3(-1.0, 1.0, 1.0),
-
-	vec3(-1.0,-1.0,-1.0),
-	vec3( 1.0,-1.0,-1.0),
-	vec3( 1.0, 1.0,-1.0),
-	vec3(-1.0, 1.0,-1.0)
+layout (location=0) out vec2 uv;
+const vec2 pos[3] = vec2[3](
+	vec2(-0.6f, -0.4f),
+	vec2( 0.6f, -0.4f),
+	vec2( 0.0f,  0.6f)
 );
-const vec3 col[8] = vec3[8](
-	vec3( 1.0, 0.0, 0.0),
-	vec3( 0.0, 1.0, 0.0),
-	vec3( 0.0, 0.0, 1.0),
-	vec3( 1.0, 1.0, 0.0),
-
-	vec3( 1.0, 1.0, 0.0),
-	vec3( 0.0, 0.0, 1.0),
-	vec3( 0.0, 1.0, 0.0),
-	vec3( 1.0, 0.0, 0.0)
-);
-const int indices[36] = int[36](
-	// front
-	0, 1, 2, 2, 3, 0,
-	// right
-	1, 5, 6, 6, 2, 1,
-	// back
-	7, 6, 5, 5, 4, 7,
-	// left
-	4, 0, 3, 3, 7, 4,
-	// bottom
-	4, 5, 1, 1, 0, 4,
-	// top
-	3, 2, 6, 6, 7, 3
+const vec2 tc[3] = vec2[3](
+	vec2( 0.0, 0.0 ),
+	vec2( 1.0, 0.0 ),
+	vec2( 0.5, 1.0 )
 );
 void main()
 {
-	int idx = indices[gl_VertexID];
-	gl_Position = MVP * vec4(pos[idx], 1.0);
-	color = isWireframe > 0 ? vec3(0.0) : col[idx];
+	gl_Position = MVP * vec4(pos[gl_VertexID], 0.0, 1.0);
+	uv = tc[gl_VertexID];
 }
 )";
 
-static const char* shaderCodeFragment = R"(
+static const char *shaderCodeFragment = R"(
 #version 460 core
-layout (location=0) in vec3 color;
+layout (location=0) in vec2 uv;
 layout (location=0) out vec4 out_FragColor;
+uniform sampler2D texture0;
 void main()
 {
-	out_FragColor = vec4(color, 1.0);
+	out_FragColor = texture(texture0, uv);
 };
 )";
-
-struct PerFrameData
-{
-	mat4 mvp;
-	int isWireframe;
-};
 
 int main(void)
 {
 	glfwSetErrorCallback(
-		[](int error, const char* description)
+		[](int error, const char *description)
 		{
 			fprintf(stderr, "Error: %s\n", description);
-		}
-	);
+		});
 
 	if (!glfwInit())
 		exit(EXIT_FAILURE);
@@ -93,7 +65,7 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(1024, 768, "Simple example", nullptr, nullptr);
+	GLFWwindow *window = glfwCreateWindow(1024, 768, "Simple example", nullptr, nullptr);
 	if (!window)
 	{
 		glfwTerminate();
@@ -102,12 +74,20 @@ int main(void)
 
 	glfwSetKeyCallback(
 		window,
-		[](GLFWwindow* window, int key, int scancode, int action, int mods)
+		[](GLFWwindow *window, int key, int scancode, int action, int mods)
 		{
 			if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 				glfwSetWindowShouldClose(window, GLFW_TRUE);
-		}
-	);
+			if (key == GLFW_KEY_F9 && action == GLFW_PRESS)
+			{
+				int width, height;
+				glfwGetFramebufferSize(window, &width, &height);
+				uint8_t *ptr = (uint8_t *)malloc(width * height * 4);
+				glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, ptr);
+				stbi_write_png("screenshot.png", width, height, 4, ptr, 0);
+				free(ptr);
+			}
+		});
 
 	glfwMakeContextCurrent(window);
 	gladLoadGL(glfwGetProcAddress);
@@ -125,30 +105,34 @@ int main(void)
 	glAttachShader(program, shaderVertex);
 	glAttachShader(program, shaderFragment);
 	glLinkProgram(program);
-	glUseProgram(program);
 
 	GLuint vao;
 	glCreateVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
-	const GLsizeiptr kBufferSize = sizeof(PerFrameData);
+	const GLsizeiptr kBufferSize = sizeof(mat4);
 
-	// Direct-State-Access
 	GLuint perFrameDataBuffer;
 	glCreateBuffers(1, &perFrameDataBuffer);
-	// The GL_DYNAMIC_STORAGE_BIT parameter tells the OpenGL implementation that the content of 
-	// the data store might be updated after creation through calls to glBufferSubData()
 	glNamedBufferStorage(perFrameDataBuffer, kBufferSize, nullptr, GL_DYNAMIC_STORAGE_BIT);
-	//binds a range within a buffer object to an indexed buffer target. 
 	glBindBufferRange(GL_UNIFORM_BUFFER, 0, perFrameDataBuffer, 0, kBufferSize);
 
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_POLYGON_OFFSET_LINE);
-	// Polygon offset is needed to render a wireframe image of the cube on top of the solid
-	// image without Z-fighting. The values of -1.0 will move the wireframe rendering
-	// slightly toward the camera.
-	glPolygonOffset(-1.0f, -1.0f);
+
+	int w, h, comp;
+	const uint8_t *img = stbi_load("data/stb_sample.jpg", &w, &h, &comp, 3);
+
+	GLuint texture;
+	glCreateTextures(GL_TEXTURE_2D, 1, &texture);
+	glTextureParameteri(texture, GL_TEXTURE_MAX_LEVEL, 0);
+	glTextureParameteri(texture, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTextureParameteri(texture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTextureStorage2D(texture, 1, GL_RGB8, w, h);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTextureSubImage2D(texture, 0, 0, 0, w, h, GL_RGB, GL_UNSIGNED_BYTE, img);
+	glBindTextures(0, 1, &texture);
+
+	stbi_image_free((void *)img);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -157,28 +141,21 @@ int main(void)
 		const float ratio = width / (float)height;
 
 		glViewport(0, 0, width, height);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-		const mat4 m = glm::rotate(glm::translate(mat4(1.0f), vec3(0.0f, 0.0f, -3.5f)), (float)glfwGetTime(), vec3(1.0f, 1.0f, 1.0f));
-		const mat4 p = glm::perspective(45.0f, ratio, 0.1f, 1000.0f);
+		const mat4 m = glm::rotate(mat4(1.0f), (float)glfwGetTime(), vec3(0.0f, 0.0f, 1.0f));
+		const mat4 p = glm::ortho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+		const mat4 mvp = p * m;
 
-		PerFrameData perFrameData = { .mvp = p * m, .isWireframe = false };
-
-		glNamedBufferSubData(perFrameDataBuffer, 0, kBufferSize, &perFrameData);
-		// render the solid cube with the polygon mode set to GL_FILL
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		perFrameData.isWireframe = true;
-		glNamedBufferSubData(perFrameDataBuffer, 0, kBufferSize, &perFrameData);
-		//render the wireframe cube using the GL_LINE polygon mode and the -1.0 polygon offset 
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glUseProgram(program);
+		glNamedBufferSubData(perFrameDataBuffer, 0, kBufferSize, glm::value_ptr(mvp));
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glDeleteTextures(1, &texture);
 	glDeleteBuffers(1, &perFrameDataBuffer);
 	glDeleteProgram(program);
 	glDeleteShader(shaderFragment);
