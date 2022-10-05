1f076d39e51ba510d7d5f90e96ba8beb3318167b
jsjtxietian <jsjtxietian@outlook.com>
2022-10-05 17:02:12 +0800

    grid shader

---
+++ data/shaders/GridParameters.h
@@ -0,0 +1,27 @@
+﻿// extents of grid in world coordinates
+// how far from the camera the grid will be visible:
+float gridSize = 100.0;
+
+// size of one cell
+float gridCellSize = 0.025;
+
+// color of thin lines
+vec4 gridColorThin = vec4(0.5, 0.5, 0.5, 1.0);
+
+// color of thick lines (every tenth line)
+vec4 gridColorThick = vec4(0.0, 0.0, 0.0, 1.0);
+
+// minimum number of pixels between cell lines before LOD switch should occur.
+// switch the LOD when the number of pixels between two adjacent cell lines drops below this value 
+const float gridMinPixelsBetweenCells = 2.0;
+
+const vec3 pos[4] = vec3[4](
+	vec3(-1.0, 0.0, -1.0),
+	vec3( 1.0, 0.0, -1.0),
+	vec3( 1.0, 0.0,  1.0),
+	vec3(-1.0, 0.0,  1.0)
+);
+
+const int indices[6] = int[6](
+	0, 1, 2, 2, 3, 0
+);
