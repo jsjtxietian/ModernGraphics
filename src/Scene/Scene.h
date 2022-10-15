43894feb5294b979cacf2e6d1363eac5ed352d58
jsjtxietian <jsjtxietian@outlook.com>
2022-10-16 00:25:46 +0800

    Scene converter

--- src/Scene/Scene.h
+++ src/Scene/Scene.h
@@ -60,3 +60,40 @@ struct Scene
     // Debug list of material names
     std::vector<std::string> materialNames_;
 };
+
+int addNode(Scene &scene, int parent, int level);
+
+void markAsChanged(Scene &scene, int node);
+
+int findNodeByName(const Scene &scene, const std::string &name);
+
+inline std::string getNodeName(const Scene &scene, int node)
+{
+    int strID = scene.nameForNode_.contains(node) ? scene.nameForNode_.at(node) : -1;
+    return (strID > -1) ? scene.names_[strID] : std::string();
+}
+
+inline void setNodeName(Scene &scene, int node, const std::string &name)
+{
+    uint32_t stringID = (uint32_t)scene.names_.size();
+    scene.names_.push_back(name);
+    scene.nameForNode_[node] = stringID;
+}
+
+int getNodeLevel(const Scene &scene, int n);
+
+void recalculateGlobalTransforms(Scene &scene);
+
+void loadScene(const char *fileName, Scene &scene);
+void saveScene(const char *fileName, const Scene &scene);
+
+void dumpTransforms(const char *fileName, const Scene &scene);
+void printChangedNodes(const Scene &scene);
+
+void dumpSceneToDot(const char *fileName, const Scene &scene, int *visited = nullptr);
+
+void mergeScenes(Scene &scene, const std::vector<Scene *> &scenes, const std::vector<glm::mat4> &rootTransforms, const std::vector<uint32_t> &meshCounts,
+                 bool mergeMeshes = true, bool mergeMaterials = true);
+
+// Delete a collection of nodes from a scenegraph
+void deleteSceneNodes(Scene &scene, const std::vector<uint32_t> &nodesToDelete);
