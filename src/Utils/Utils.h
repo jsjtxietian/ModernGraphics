b7b781ecf96772ed9aca84a2454ccb84c5f0057a
jsjtxietian <jsjtxietian@outlook.com>
2022-10-18 19:35:36 +0800

    add merge util

--- src/Utils/Utils.h
+++ src/Utils/Utils.h
@@ -12,6 +12,7 @@ inline void mergeVectors(std::vector<T> &v1, const std::vector<T> &v2)
     v1.insert(v1.end(), v2.begin(), v2.end());
 }
 
+// the index of a previously added texture file is returned
 inline int addUnique(std::vector<std::string> &files, const std::string &file)
 {
     if (file.empty())
